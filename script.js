82d25738803cebbf938adc72b0a28678fef00bbf
Emdadul Islam <bekon1700@gmail.com>
2022-09-15 00:58:33 +0600

    count solve

--- script.js
+++ script.js
@@ -113,11 +113,14 @@ const start = () => {
     if (count == 0) {
       // -------------- START TYPING -----------------
       document.addEventListener("keydown", typeController);
-      countdownOverlay.style.display = "flex";
+      countdownOverlay.style.display = "none";
       display.classList.remove("inactive");
 
       clearInterval(startCountdown);
       startTime = new Date().getTime();
+    }
+    console.log(count)
+    if(count == -1){
     }
     count--;
   }, 1000);
